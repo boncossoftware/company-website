5db5da805b4779036591912d3d1a20ec7c5d8279
Israel A. Posner <israelposner@boncos.io>
2021-04-14 15:16:34 -0400

    Added firebase

--- src/components/footer.test.tsx
+++ src/components/footer.test.tsx
@@ -3,4 +3,14 @@ import Footer from './footer';
 
 test('renders correctly', () => {
   render(<Footer />);
+  
+  const year = (new Date()).getFullYear();
+  const copyright = screen.getByText(`Copyright © ${year} Posco Enterprises VBA. All rights reserved.`);
+  expect(copyright).toBeInTheDocument();
+
+  const chamber = screen.getByText('KvK 45207.0');
+  expect(chamber).toBeInTheDocument();
+
+  const place = screen.getByText('Paradera, Aruba');
+  expect(place).toBeInTheDocument();
 });
